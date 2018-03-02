719ace5dd730d751b56073189d59a95b3247d597
Camilo Diaz <caadiazsi@unal.edu.co>
2018-03-02 14:27:29 -0500

    Aproximate Time

--- Matrix_c--.cpp
+++ Matrix_c--.cpp
@@ -5,30 +5,35 @@
 using namespace std;
 
 int main(){
-	int N;
-	cin>>N;
-	int Matrix_A[N][N];
-	int Matrix_B[N][N];
-	int Matrix_AB[N][N];
-	int Operations = N*N*((2*N)-1);
-	cout<< Operations <<endl;
-	for (int i=0;i<N;i++){
-		for (int j=0;j<N;j++){
-			Matrix_A[i][j] = 1;
-			Matrix_B[i][j] = 2;
-		}
-	}
-	clock_t Init = clock();
-	for (int i=0;i<N;i++){
-		for (int j=0;j<N;j++){
-			int count = 0;
-			for (int k=0;k<N;k++){
-				count += Matrix_A[i][k]*Matrix_B[k][j];
+	for (int N=50;N<=450;N+=50){
+		int Matrix_A[N][N];
+		int Matrix_B[N][N];
+		int Matrix_AB[N][N];
+		int Operations = N*N*((2*N)-1);
+		cout<<"Size: "<< N <<endl;
+		cout<<"Operations: " << Operations <<endl;
+		for (int i=0;i<N;i++){
+			for (int j=0;j<N;j++){
+				Matrix_A[i][j] = 1;
+				Matrix_B[i][j] = 2;
 			}
-			Matrix_AB[i][j] = count;
 		}
-	}
-	double execution_Time = (double)(clock() - Init)/CLOCKS_PER_SEC ;
-	cout<<execution_Time/Operations<<endl;
+		long double execution_Time = 0;
+		clock_t Init = clock();
+		for (int times=0;times<10;times++){ 
+			for (int i=0;i<N;i++){
+				for (int j=0;j<N;j++){
+					int count = 0;
+					for (int k=0;k<N;k++){
+						count += Matrix_A[i][k]*Matrix_B[k][j];
+					}
+					Matrix_AB[i][j] = count;
+				}
+			}
+		}
+		execution_Time = (long double)(clock() - Init)/CLOCKS_PER_SEC ;
+		cout<<"Execution Time: "<<execution_Time/(Operations*10)<<endl;
+		cout<<"----------------------------------------"<<endl;
+	}	
 	return 0;
 }
